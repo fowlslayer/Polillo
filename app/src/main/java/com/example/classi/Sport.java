dc110938d85bb0a0284554a90ae5375c4fdad0aa
Easlay <norberto@scaravaggi.net>
2015-06-08 20:20:03 -0700

    ggg

--- app/src/main/java/com/example/classi/Sport.java
+++ app/src/main/java/com/example/classi/Sport.java
@@ -1,6 +1,7 @@
 package com.example.classi;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.webkit.WebView;
@@ -11,15 +12,19 @@ public class Sport extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sport);
+    }
 
-        final WebView webDote = (WebView) findViewById(R.id.webDote);
-        final ImageButton comunicati = (ImageButton) findViewById(R.id.imageButtonDote);
-        comunicati.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                webDote.loadUrl("http://www.comune.lissone.mb.it/flex/cm/pages/ServeBLOB.php/L/IT/IDPagina/6745");
-            }
-        });
+    public void onButtonClick(View v) {
+        Intent c = new Intent(Sport.this, Web.class);
 
+
+        switch (v.getId()) {
+            case R.id.ferrari:
+                c.putExtra("url", "http://www.gazzetta.it/Formula-1/08-06-2015/f1-ferrari-austria-12080165893.shtml");
+                break;
+            case R.id.gp:
+                c.putExtra("url", "http://www.gazzetta.it/Formula-1/07-06-2015/f1-mercedes-hamilton-rosberg-12076284434.shtml");
+                break;
+        }
     }
 }
